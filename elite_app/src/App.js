5951f0162aa8add56ff0004c4360eddb3410eac3
suame2021 <85235483+suame2021@users.noreply.github.com>
2022-09-13 12:20:55 +0100

    nav bar

--- elite_app/src/App.js
+++ elite_app/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./homeComponent";
+import NavbarComponent from "./navbarComponent";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <NavbarComponent/>
+
+
+
 
         
       </div>
-     
     </div>
   );
 }
---
+++ elite_app/src/navbarComponent.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "./Images/logo.png";
+import Image from 'react-bootstrap/Image';
+
+
+const NavbarComponent = () => {
+
+  return (
+    <Container classname='Navigation'>
+      <Image src={logo} className="logo" />
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Nav className="NavBar">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Schedule</Nav.Link>
+            <Nav.Link href="#pricing">Learning</Nav.Link>
+            <Nav.Link href="#features">Profile</Nav.Link>
+            <Nav.Link href="#pricing">Notification</Nav.Link>
+            <Nav.Link href="#features">Login in</Nav.Link>
+            <Nav.Link href="#pricing">Sign Up</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+}
+ 
+export default NavbarComponent;
